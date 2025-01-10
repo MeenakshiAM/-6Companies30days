1cd1707de1753b04f7c93bc32bffa60bb69a9989
MeenakshiAM <miniaalayam@gmail.com>
2025-01-10 19:57:10 +0530

    just a small change

--- 299. Bulls and Cows day6/Solution.java
+++ 299. Bulls and Cows day6/Solution.java
@@ -2,15 +2,11 @@ class Solution {
     public String getHint(String secret, String guess) {
         int bulls = 0;
         int cows = 0;
-
-        // Count bulls and track the frequency of unmatched digits
-        int[] secretCount = new int[10];  // To store frequency of digits in secret
-        int[] guessCount = new int[10];   // To store frequency of digits in guess
-
-        // Step 1: Calculate bulls and collect unmatched digits
+        int[] secretCount = new int[10]; 
+        int[] guessCount = new int[10];
         for (int i = 0; i < secret.length(); i++) {
             if (secret.charAt(i) == guess.charAt(i)) {
-                bulls++;  // Exact match
+                bulls++; 
             } else {
                 secretCount[secret.charAt(i) - '0']++;
                 guessCount[guess.charAt(i) - '0']++;
